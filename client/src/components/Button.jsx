54316d6919fffb6bb0437d31da38f8d8db50c9da
eddybruv <edwinajong@gmail.com>
2022-06-07 20:28:32 +0100

    added event listener

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link} from 'react-router-dom'
 import './componentStyles/button.css';
 
-const Button = ({btnText, path}) => {
+const Button = ({btnText, path, handleAction}) => {
   return (
     // <button>{btnText}</button>
-    <Link className="link" to={`${path}`}><button>{btnText}</button></Link>
+    <Link className="link" to={`${path}`}><button onClick={handleAction}>{btnText}</button></Link>
   )
 }
 export default Button
