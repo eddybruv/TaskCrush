a5685721eff86878ab30b75362c07bed81808307
eddybruv <edwinajong@gmail.com>
2022-06-07 15:46:27 +0100

    created express server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+const UserRoute = require("./routes/User.route");
+const BusRoute = require("./routes/Bus.route");
+const TripRoute = require("./routes/Trip.route");
+
+const connection = mongoose.connect(
+  "mongodb+srv://Sonia:ZENNIA0709@cluster0.hcmtn.mongodb.net/BusDB?retryWrites=true&w=majority"
+);
+
+connection.then(() => console.log("Database connected"));
+connection.catch(() => console.log("Database not connected"));
+
+app.use(cors());
+
+app.use("/api/user", UserRoute);
+app.use("api/bus", BusRoute);
+app.use("/api/trip", TripRoute);
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
